09f3fafd0e0bcab12c88fadd2ed05d500e1f6ec0
OseenM <oseenmarcelino.6@gmail.com>
2025-02-14 22:56:50 -0500

    Add: NIC and Vm Version Tests

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "b40c6add-0fa1-4055-9718-f3d28641e566"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "marc",
 		},
 	}
 
@@ -30,7 +30,28 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	// Run `terraform output` to get the value of output variable
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
-
+	nicName := terraform.Output(t, terraformOptions, "nic_name")
+	imagePublisher := terraform.Output(t, terraformOptions, "vm_image_publisher")
+	imageOffer := terraform.Output(t, terraformOptions, "vm_image_offer")
+	imageSKU := terraform.Output(t, terraformOptions, "vm_image_sku")
+	imageVersion := terraform.Output(t, terraformOptions, "vm_image_version")
+	// Expected values from Terraform configuration
+	expectedPublisher := "Canonical"
+	expectedOffer := "0001-com-ubuntu-server-jammy"
+	expectedSKU := "22_04-lts-gen2"
+	expectedVersion := "latest"
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// Confirm NIC exists and is connected to the VM
+	nicExists := azure.NetworkInterfaceExists(t, nicName, resourceGroupName, subscriptionID)
+	t.Logf("Checking if NIC %s exists in resource group %s", nicName, resourceGroupName)
+	assert.True(t, nicExists, "Network Interface should exist")
+
+	// Validate the expected image details
+	assert.Equal(t, expectedPublisher, imagePublisher, "VM should be from Canonical")
+	assert.Equal(t, expectedOffer, imageOffer, "VM offer should be 0001-com-ubuntu-server-jammy")
+	assert.Equal(t, expectedSKU, imageSKU, "VM should be running Ubuntu 22.04 LTS")
+	assert.Equal(t, expectedVersion, imageVersion, "VM verison should be latest")
 }
+	
