91a3419a1e493f24679ff19da0fde28efabb1dce
OseenM <oseenmarcelino.6@gmail.com>
2025-02-14 22:56:36 -0500

    Add output for tests

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,19 @@ output "nic_name" {
 output "public_ip" {
   value = azurerm_linux_virtual_machine.webserver.public_ip_address
 }
+
+output "vm_image_publisher" {
+  value = azurerm_linux_virtual_machine.webserver.source_image_reference[0].publisher
+}
+
+output "vm_image_offer" {
+  value = azurerm_linux_virtual_machine.webserver.source_image_reference[0].offer
+}
+
+output "vm_image_sku" {
+  value = azurerm_linux_virtual_machine.webserver.source_image_reference[0].sku
+}
+
+output "vm_image_version" {
+  value = azurerm_linux_virtual_machine.webserver.source_image_reference[0].version
+}
